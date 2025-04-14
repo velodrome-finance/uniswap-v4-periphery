8d1135f669ef3be22ebedf9042eef778b0348779
simplyoptimistic <111120814+simplyoptimistic@users.noreply.github.com>
2025-04-14 18:57:22 +1000

    feat: update to v0.8.28, update v4-core lib

--- src/PositionManager.sol
+++ src/PositionManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity >=0.8.26 <0.9.0;
 
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
--- src/UniswapV4DeployerCompetition.sol
+++ src/UniswapV4DeployerCompetition.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity >=0.8.26 <0.9.0;
 
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 import {VanityAddressLib} from "./libraries/VanityAddressLib.sol";
